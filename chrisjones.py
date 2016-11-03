7a5507ee71f157369777c491348a033631de9753
ngparas <nickgp@gmail.com>
2016-11-03 18:31:02 -0500

    started pulling sentences

--- chrisjones.py
+++ chrisjones.py
@@ -2,6 +2,7 @@ import requests
 import json
 import elastic
 from google_nlp_api import GoogleNlp
+import re
 
 
 
@@ -14,15 +15,17 @@ class ChrisJones:
         # determine question type
         # then generate appropriate response
 
-        print query
+        # print query
 
         a = GoogleNlp()
-        print a.annotate_text(query)
+        annotated_query = a.annotate_text(query)
+        print annotated_query
+
 
         # for now, just return the same stuff all the time
         payload = {"query": {"query_string": {"query": query.encode('utf-8'),
                                           "fields": ["Full text:"]}}}
-        response = elastic.search(elastic.ES_URL, '_search', payload)
+        response = elastic.search(elastic.ES_URL, 'articles/_search', payload)
         response = json.loads(response)
 
         hits = response['hits']['hits']
@@ -31,9 +34,16 @@ class ChrisJones:
 
         for a in hits:
             if a["_score"] == max_score:
-                return a["_source"]['Full text:']
+                match_id = a["_id"]
+                # return a["_source"]['Full text:']
 
-        return response
+        payload = {"query": {"ids": {"values": [match_id]}}}
+        annotated_article = elastic.search(elastic.ES_URL, 'googles/_search', payload)
+        annotated_article = json.loads(annotated_article)
+        sentences = annotated_article['hits']['hits'][0]['_source']['sentences']
+        sentences = [i['text']['content'] for i in sentences]
+        sentences = [i for i in sentences if re.search('Hamilton', i) != None]
+        return sentences[len(sentences) - 2]
 
 
 

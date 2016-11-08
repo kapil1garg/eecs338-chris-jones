85130b4f22a99d06e245374156d4ef8ac78aaa46
Kapil Garg <kapil1garg@gmail.com>
2016-11-08 01:53:06 -0600

    cleaning up data cleaning scripts

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,67 +1,127 @@
+"""
+This module is used to clean raw data retieved from Nexis
+"""
 import re
 
 def split_article(article):
-  metadata = []
-  full_text = []
-  hit_metadata = False
-  regex = r'Title: '
-  for line in article:
-    if re.match(regex, line):
-      hit_metadata = True
-    if hit_metadata:
-      metadata.append(line)
-    else:
-      full_text.append(line)
+    """
+    Splits single article into full text and metadata
 
-  return full_text, metadata
+    input:
+        article (string): single article from raw data
+
+    outputs:
+        (string): full text
+        (string): metadata
+    """
+    metadata = []
+    full_text = []
+    hit_metadata = False
+    regex = r'Title: '
+    for line in article:
+        if re.match(regex, line):
+            hit_metadata = True
+        if hit_metadata:
+            metadata.append(line)
+        else:
+            full_text.append(line)
+
+    return full_text, metadata
 
 def write(lines, title):
-  f = open(title, 'w')
-  for line in lines:
-    f.write(line)
-  f.close()
+    """
+    Writes file to disk
+
+    input:
+        lines (list): individual lines in file to write
+        title (string): file name
+    """
+    output_file = open(title, 'w')
+    for line in lines:
+        output_file.write(line)
+    output_file.close()
 
 def write_article_to_file(article, index):
-  full_text, metadata = split_article(article)
-  write(full_text, "clean_data/full_text/" + index)
-  write(metadata, "clean_data/metadata/" + index)
+    """
+    Parses out article from large file and exports file
+
+    input:
+        article (string): article text
+        index (int): index of article loaded in
+    """
+    full_text, metadata = split_article(article)
+    write(full_text, 'clean_data/full_text/' + index)
+    write(metadata, 'clean_data/metadata/' + index)
 
 def find_articles(sections):
-  regex = r'Document \d+ of \d+'
-  articles = []
-  for section in sections:
-    if re.match(regex, section[1]):
-      articles.append(section)
-  return articles
+    """
+    Parses large file into sections
+
+    input:
+        sections (list): list of sections
+
+    output:
+        (list): articles parsed into list
+    """
+    regex = r'Document \d+ of \d+'
+    articles = []
+    for section in sections:
+        if re.match(regex, section[1]):
+            articles.append(section)
+    return articles
 
 def find_sections(file_name):
-  f = open(file_name)
-  text = f.readlines()
+    """
+    Parses different sections out
 
-  BREAK = "____________________________________________________________\r\n"
+    input:
+        file_name (string): file to parse sections from
 
-  current_section = []
-  all_sections = []
-  for line in text:
-    if line != BREAK:
-      current_section.append(line)
-    elif line == BREAK:
-      if len(current_section) > 0:
-        all_sections.append(current_section)
-        current_section = []
-  all_sections.append(current_section)
+    output:
+        (list): list of parsed sections
+    """
+    input_file = open(file_name)
+    text = input_file.readlines()
 
-  return all_sections
+    break_text = "____________________________________________________________\r\n"
+
+    current_section = []
+    all_sections = []
+    for line in text:
+        if line != break_text:
+            current_section.append(line)
+        elif line == break_text:
+            if len(current_section) > 0:
+                all_sections.append(current_section)
+                current_section = []
+    all_sections.append(current_section)
+
+    return all_sections
 
 def write_file(file_name, i):
-  sections = find_sections(file_name)
-  articles = find_articles(sections)
-  for j in range(0, len(articles)):
-    index = str(i) + str(j) + '.txt'
-    write_article_to_file(articles[j], index)
+    """
+    Parses sections and then calls file writer for text and metadata
+
+    inputs:
+        file_name (string): file to load in
+        i (int): file index to be used as file name
+    """
+    sections = find_sections(file_name)
+    articles = find_articles(sections)
+
+    for j in range(0, len(articles)):
+        index = str(i) + str(j) + '.txt'
+        write_article_to_file(articles[j], index)
+
+def main():
+    """
+    Called when module is called from command line
+    """
+    base_file_name = 'ChrisJones-'
+
+    for i in range(0, 9):
+        file_name = 'raw_data/' + base_file_name + str(i) +'.txt'
+        write_file(file_name, i)
 
 if __name__ == '__main__':
-  base_file_name = "ChrisJones-"
-  for i in range(0, 9):
-    file_name = 'raw_data/ChrisJones-' + str(i) +'.txt'
-    write_file(file_name, i)
+    main()

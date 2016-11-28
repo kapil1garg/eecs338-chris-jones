8ed794f8988d26f1afdb0f63dc73c4ae255a5b1a
ngparas <nickgp@gmail.com>
2016-11-28 01:12:07 -0600

    deleted extra print statement in QueryAnalyzer

--- QueryAnalyzer.py
+++ QueryAnalyzer.py
@@ -77,7 +77,6 @@ class AnnotatedQuery:
       q = q.replace(p, 'PERSON')
     for g in self.genres:
       q = q.replace(g, 'GENRE')
-    print q
     for n in self.keywords['keywords']['NOUN']:
       q = q.replace(n, 'NOUN')
     return q

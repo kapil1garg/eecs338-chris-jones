23c5260ed52a6a6505075d3444a4ecaa976351f5
ngparas <nickgp@gmail.com>
2016-11-27 22:37:11 -0600

    added AnnotatedQuery helper class

--- QueryAnalyzer.py
+++ QueryAnalyzer.py
@@ -1,5 +1,6 @@
 from google_nlp_api import GoogleNlp
 import pdb
+import re
 
 class QueryAnalyzer:
   """
@@ -8,9 +9,13 @@ class QueryAnalyzer:
 
   def __init__(self):
     self.google_nlp = GoogleNlp()
-    self.genres = ['comedy', 'drama', 'tragedy', 'satire', 'farce', 'musicals']
+    self.genres = ['comedy', 'drama', 'tragedy', 'satire', 'farce', 'musicals', 'musical']
     with open('data/theaters.txt', 'r') as theater_file:
       self.theaters = theater_file.read().splitlines()
+    with open('data/people.txt', 'r') as people_file:
+      self.people = people_file.read().splitlines()
+    with open('data/shows.txt', 'r') as shows_file:
+      self.shows = shows_file.read().splitlines()
 
   def get_entities(self, query):
     """
@@ -43,21 +48,8 @@ class QueryAnalyzer:
       if token['dependencyEdge']['label'] == "NSUBJ":
         return token['text']['content']
 
-  def analyze_dependencies(self, query):
-    print "Not yet implemented"
-
-  def annotate_query(self, query):
-    print "Not yet implemented"
-
   def get_keywords(self, query):
     return_blob = self.google_nlp.analyze_syntax(query)
-    keywords = {'keywords': {'NOUN': [], 'VERB': []}, 'edges': {'ROOT': [], 'NSUBJ': [], 'POBJ': [], 'ATTR': []}}
-    for token in return_blob['tokens']:
-      if token['partOfSpeech']['tag'] in keywords['keywords'].keys():
-        keywords['keywords'][token['partOfSpeech']['tag']].append(token['text']['content'])
-      if token['dependencyEdge']['label'] in keywords['edges'].keys():
-        keywords['edges'][token['dependencyEdge']['label']].append(token['text']['content' ])
-    return keywords
 
   def get_framework(self, query, keywords):
     for t in self.theaters:
@@ -68,9 +60,42 @@ class QueryAnalyzer:
     #   query = query.replace(n, 'NOUN')
     return query
 
+  def annotate(self, query):
+    return AnnotatedQuery(query, self.shows, self.theaters, self.people)
 
-# class AnnotatedQuery:
-#   def __init__(self, ):
+
+class AnnotatedQuery:
+  """
+  Helper class to get clean up syntax when working with query annotations
+
+  args:
+    query (string): the text query to annotate with the Google Natural Language API
+    shows (list): the list of shows (strings)
+    theaters (list): the list of theaters (strings)
+    people (list): the list of people (strings)
+
+  """
+  def __init__(self, query, shows, theaters, people):
+    self.api_client = GoogleNlp()
+    self.api_results = self.api_client.annotate_text(query)
+    self.keywords = {'keywords': {'NOUN': [], 'VERB': []}, 'edges': {'ROOT': [], 'NSUBJ': [], 'POBJ': [], 'ATTR': []}}
+    for token in self.api_results['tokens']:
+      if token['partOfSpeech']['tag'] in self.keywords['keywords'].keys():
+        self.keywords['keywords'][token['partOfSpeech']['tag']].append(token['text']['content'])
+      if token['dependencyEdge']['label'] in self.keywords['edges'].keys():
+        self.keywords['edges'][token['dependencyEdge']['label']].append(token['text']['content' ])
+    self.shows = []
+    self.theaters = []
+    self.people = []
+    for s in shows:
+      if re.search(s, query) != None:
+        self.shows.append(s)
+    for t in theaters:
+      if re.search(t, query) != None:
+        self.theaters.append(t)
+    for p in people:
+      if re.search(p, query) != None:
+        self.people.append(p)
 
 
 if __name__ == '__main__':
